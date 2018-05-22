2ba95ae68a3675dd3e26072f5915025627cd9a2e
Daniel Azevedo Marques <harugrin@users.noreply.github.com>
2018-05-22 12:15:05 -0300

    Initial upload.

---
+++ RHELApi/RHELApi/Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RHELApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly Dictionary<int, string> data;
+
+        public ValuesController()
+        {
+            data = new Dictionary<int, string>();
+            data.Add(1, "Simple test for RHEL 7 x64 container use.");
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new OkObjectResult(data);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var result = data.FirstOrDefault(d => d.Key.Equals(id)).Value;
+
+            if (string.IsNullOrEmpty(result))
+                return new NotFoundObjectResult($"No match for ID: \"{id}\"!");
+
+            return new OkObjectResult(result);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]string value)
+        {
+            return new StatusCodeResult(403);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]string value)
+        {
+            return new StatusCodeResult(403);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return new StatusCodeResult(403);
+        }
+    }
+}
